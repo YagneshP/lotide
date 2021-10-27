52ea88749591b934f3cd062575199c92b35f5c67
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-10-27 18:46:01 -0400

    comparing keys array length of objects

--- eqObjects.js
+++ eqObjects.js
@@ -10,5 +10,18 @@ const assertEqual = function(actual, expected) {
 //Otherwise false
 
 const eqObjects = function(obj1, obj2) {
-  
-}
+  if (Object.keys(obj1).length === Object.keys(obj2).length) {
+    return true;
+  }
+  return false;
+};
+
+
+//Test
+const ab = {a : 'a', b : 'b'};
+const ba = {b : 'b', a : 'a'};
+const abc = {a : 'a', b : 'b', c : 'c'};
+
+assertEqual(eqObjects(ab,ba), true);
+assertEqual(eqObjects(abc, ab), false);
+assertEqual(eqObjects(ab,ba), undefined);
