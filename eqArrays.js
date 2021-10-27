ce44b2ad537079cacd17a36d42b1afa599e5574a
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-10-26 20:15:28 -0400

    fix : linting errors

--- eqArrays.js
+++ eqArrays.js
@@ -7,22 +7,23 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if(arr1.length === arr2.length){
-    if(arr1.length === 0){
-      return true
+  if (arr1.length === arr2.length) {
+    if (arr1.length === 0) {
+      return true;
     } else {
       let result = [];
-      for(let i = 0; i < arr1.length; i++){
+      for (let i = 0; i < arr1.length; i++) {
         result.push(arr1[i] === arr2[i]);
       }
-      return !result.includes(false)
+      return !result.includes(false);
     }
   } else {
-    return false
+    return false;
   }
-}
+};
 
-// Test 
+//Test//
+
 assertEqual(eqArrays([1,2,3],[1,2,3]),true);
 assertEqual(eqArrays([1,2,3],[1,2,'3']),true);
 assertEqual(eqArrays([1,2,3],[1,2,'3']),false);
