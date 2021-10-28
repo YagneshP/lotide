d370913a89de079c5231d640d49afea896faf845
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-10-28 17:12:13 -0400

    implementin for of,

--- takeUntil.js
+++ takeUntil.js
@@ -1,14 +1,39 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed : ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed : ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+//---- takeUntil ------//
+
+
 const takeUntil = function(array, callback) {
   // return slice of input array
   // until callback find the truthy value
   // loop through each element
   // stop where condition matched
+  
   let result = [];
-  for(let i = 0; i < array.length; i++){
-    if(callback(array[i])){
+  for(let val of array){
+    if(callback(val)){
       break;
     }
-    result.push(array[i]);
+    result.push(val);
   }
   return result;
 }
@@ -27,3 +52,8 @@ console.log(results2);
 //[ 1, 2, 5, 7, 2 ]
 // ---
 // [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+assertArrayEqual(results1, [1,2,5,7,2]);
+assertArrayEqual(results1, []);
+assertArrayEqual(results1, [1, 2, 5, 7, 2, -1]);
+assertArrayEqual(results1,data1);
