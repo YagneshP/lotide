64f364178875b5517156611914159a32f365d725
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-01 15:04:10 -0400

    refactor test case for headTest with help of mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(['Hello']),'Hello');
-assertEqual(head([1,2,3]), 4);
+describe("#head", () => {
+  it('returns 1 for [1,2,3]', () => {
+    assert.strictEqual(head([1,2,3]),1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("should not return 4 for [1,2,4]", () =>{
+    assert.notEqual(head([1,2,4]),4);
+  })
+})
