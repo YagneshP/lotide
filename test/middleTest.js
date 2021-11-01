3e704f6c110bccf073917f8b9bc97dd900ccec59
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-01 15:49:08 -0400

    refactor test cases with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,42 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArrayEqual = require('../assertArraysEqual');
 
-//Test//
-assertArrayEqual(middle([1,2,3]),[2]);
-assertArrayEqual(middle([1,2,3,4]), [2,3]);
-assertArrayEqual(middle([]),[]);
-assertArrayEqual(middle([1,2,3,4]),[2]); //false
-assertArrayEqual(middle([1,2,3,4]),[2,3]);
-assertArrayEqual(middle([1]),[]);
-assertArrayEqual(middle([1,2]),[]);
-assertArrayEqual(middle([1,2]),[1]); //false
+describe('#middle', () => {
+  it('should returns [2] for [1,2,3]', () => {
+    let arr = [1,2,3];
+    let actual = middle(arr);
+    let expected = [2];
+    assert.deepEqual(actual,expected);
+  });
+  it('should returns [2,3] for [1,2,3,4]', () => {
+    let arr = [1,2,3,4];
+    let actual = middle(arr);
+    let expected = [2,3];
+    assert.deepEqual(actual,expected);
+  });
+  it('should returns [] for []', () => {
+    let arr = [];
+    let actual = middle(arr);
+    assert.isEmpty(actual,'An empty array yield an empty array for its middle');
+  });
+  it('should returns [] for [1]', () => {
+    let arr = [1];
+    let actual = middle(arr);
+    assert.isEmpty(actual,'An array with single element yield an empty array for its middle');
+  });
+  it('should returns [] for [1,2]', () => {
+    let arr = [1,2];
+    let actual = middle(arr);
+    assert.isEmpty(actual,'An array with only two elements yield an empty array for its middle');
+  });
+  it('should not return [2] for [1,2,3,4]',() => {
+    let arr = [1,2,3,4];
+    let actual = middle(arr);
+    assert.notDeepEqual(actual,[2]);
+  });
+  it('should not return [1] for [1,2]',() => {
+    let arr = [1,2];
+    let actual = middle(arr);
+    assert.notDeepEqual(actual,[1]);
+  });
+});
