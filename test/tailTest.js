04334fca2892723a9677c1ac124e00577face344
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-01 15:36:56 -0400

    remove unwanted comments

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,7 @@
-// const assertEqual = require('../assertEqual');
+
 const assert = require('chai').assert;
 const tail = require('../tail');
 
-// let array = [1,2,3];
-// let result = tail(array);
-// assertEqual(array.length - 1 , result.length);
-// assertEqual(2, result[0]);
-// assertEqual(3, result[1]);
-
-//test for not modifing the original array
-// assertEqual(array.length, 3);
 describe('#tail', () => {
   it('returns [2,3] for [1,2,3]',() => {
     let arr = [1,2,3];
