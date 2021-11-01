5139f103c0a9c177def00f785381e6e793381fa2
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-01 14:27:25 -0400

    refactor using module.exports

--- tail.js
+++ tail.js
@@ -1,19 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed : ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed : ${actual} !== ${expected}`);
-  }
-};
-let array = [1,2,3];
+
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-let result = tail(array);
-assertEqual(array.length - 1 , result.length);
-assertEqual(2, result[0]);
-assertEqual(3, result[1]);
-
-//test for not modifing the original array
-assertEqual(array.length, 3);
+module.exports = tail;
